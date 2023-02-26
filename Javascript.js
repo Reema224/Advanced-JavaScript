ca3905811258e54d731d52a7349cfa23c9e8b225
rima <rima.abouchakra.gms@gmail.com>
2023-02-26 21:40:47 +0200

    check if a word ispalindrome and primeNumber

--- Javascript.js
+++ Javascript.js
@@ -1,8 +1,8 @@
-const value = [];
-for (let i = 0; i < 10; i++) {
-  const number = prompt(`Add a number ${i + 1}:`);
-  value.push(parseFloat(number));
-}
+// const value = [];
+// for (let i = 0; i < 10; i++) {
+//   const number = prompt(`Add a number ${i + 1}:`);
+//   value.push(parseFloat(number));
+// }
 
 function mergeSort(array) {
   //if the array is empty or contain 1 element, so it is sorted
@@ -37,3 +37,62 @@ function mergeSort(array) {
 }
 const sortedArray = mergeSort(value);
 document.getElementById("numbers").innerHTML=sortedArray;
+
+
+
+function checkPalindrome(){
+var word=document.getElementById("word").value;
+if (Palindrome(word)) {
+  document.getElementById("palindrome").innerHTML=(`${word} is a palindrome!`);
+} else {
+   document.getElementById("palindrome").innerHTML=(`${word} is not a palindrome!`);
+}
+}
+
+function Palindrome(s) {
+  if (s.length < 2) {
+    return true;
+  } else {
+//check first and last character   
+if (s[0] !== s[s.length - 1]) {
+      return false;
+} else {
+// Apply same algorithm recursively
+      const middle = s.substring(1, s.length - 1);
+      return Palindrome(middle);
+    }
+  }
+}
+
+function checkPrime(){
+
+const x=document.getElementById("age").value;
+const yearOfBirth = parseInt(x);
+const currentYear = new Date().getFullYear();
+const age = currentYear - yearOfBirth;
+
+if (Prime(age)) {
+  document.getElementById("prime").innerHTML=(`${age} is a prime number!`);
+} else {
+   document.getElementById("prime").innerHTML=(`${age} is not a prime number!`);
+}
+}
+
+function Prime(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) {
+    
+     return false;
+    }
+  }
+  return true;
+}
+
+
+
+
+
+
